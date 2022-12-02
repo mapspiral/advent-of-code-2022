f1d0fe4cba164eaa5acf44241cfa1b0da85a87e5
Robert van Drunen <robert@mapspiral.net>
2022-12-02 20:28:29 +0100

    feat: solution for puzzle 1

---
+++ AdventOfCode.Shared/ArrayHelpers.cs
@@ -0,0 +1,15 @@
+﻿namespace AdventOfCode.Shared;
+
+public static class ArrayHelpers
+{
+    public static TType[] ParseAs<TType>(this string[] source)
+    {
+        return source.Select(e =>
+        {
+            var actualType = Nullable.GetUnderlyingType(typeof(TType));
+            return actualType != null && !string.IsNullOrEmpty(e)
+                ? Convert.ChangeType(e, actualType)
+                : default(TType);
+        }).Cast<TType>().ToArray();
+    }
+}
---
+++ AdventOfCode2022.Puzzle1/Program.cs
@@ -0,0 +1,61 @@
+﻿using AdventOfCode.Shared;
+
+int Solution1(string[] input)
+{
+    var elves = new List<Elf>();
+    var currentElf = new Elf();
+    foreach (var calories in input.ParseAs<int?>())
+    {
+        if (calories.HasValue)
+        {
+            currentElf.Add(calories.Value);            
+        }
+        else
+        {
+            elves.Add(currentElf);
+            currentElf = new Elf();
+        }
+    }
+    return elves.Max(x => x.Calories);
+}
+
+int Solution2(string[] input)
+{
+    var elves = new List<Elf>();
+    var currentElf = new Elf();
+    foreach (var calories in input.ParseAs<int?>())
+    {
+        if (calories.HasValue)
+        {
+            currentElf.Add(calories.Value);            
+        }
+        else
+        {
+            elves.Add(currentElf);
+            currentElf = new Elf();
+        }
+    }
+    return elves.OrderByDescending(x => x.Calories).Take(3).Sum(x => x.Calories);
+}
+
+var sampleInput = File.ReadAllLines("sample.txt");
+Console.WriteLine("Solution sample:");
+Console.WriteLine(Solution1(sampleInput));
+Console.WriteLine(Solution2(sampleInput));
+
+var input = File.ReadAllLines("input.txt");
+Console.WriteLine("Solution:");
+Console.WriteLine(Solution1(input));
+Console.WriteLine(Solution2(input));
+
+Console.ReadKey();
+
+internal sealed class Elf
+{
+    public int Calories { get; private set; }
+
+    public void Add(int calories)
+    {
+        Calories += calories;
+    }
+}
