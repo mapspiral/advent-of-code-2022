7a15d11b9c76cf4db884eb95f777f277d504fb72
Robert van Drunen <robert@mapspiral.net>
2022-12-05 20:11:59 +0100

    chore: add operation timer

--- AdventOfCode.Shared/PuzzleBase{TResult}.cs
+++ AdventOfCode.Shared/PuzzleBase{TResult}.cs
@@ -1,3 +1,5 @@
+using System.Diagnostics;
+
 namespace AdventOfCode.Shared;
 
 public abstract class PuzzleBase<TResult>
@@ -7,14 +9,23 @@ public abstract class PuzzleBase<TResult>
         Console.WriteLine($"{GetType()}");
         Console.WriteLine(new String('=', GetType().ToString().Length));
         Console.WriteLine("Solution 1");
-        Console.WriteLine($"\tSample: {Solution1(File.ReadAllLines("sample.txt"))}");
-        Console.WriteLine($"\tInput : {Solution1(File.ReadAllLines("input.txt"))}");
+        Console.WriteLine($"\tSample: {Timed(() => Solution1(File.ReadAllLines("sample.txt")))}");
+        Console.WriteLine($"\tInput : {Timed(() => Solution1(File.ReadAllLines("input.txt")))}");
         Console.WriteLine("Solution 2");
-        Console.WriteLine($"\tSample: {Solution2(File.ReadAllLines("sample.txt"))}");
-        Console.WriteLine($"\tInput : {Solution2(File.ReadAllLines("input.txt"))}");
+        Console.WriteLine($"\tSample: {Timed(() => Solution2(File.ReadAllLines("sample.txt")))}");
+        Console.WriteLine($"\tInput : {Timed(() => Solution2(File.ReadAllLines("input.txt")))}");
     }
 
     protected virtual TResult? Solution1(IEnumerable<string> input) => default;
 
     protected virtual TResult? Solution2(IEnumerable<string> input) => default;
+    
+    private static string Timed(Func<TResult?> operation)
+    {
+        var stopwatch = new Stopwatch();
+        stopwatch.Start();
+        var result = operation();
+        stopwatch.Stop();
+        return $"{result} ({stopwatch.Elapsed})";
+    }
 }
