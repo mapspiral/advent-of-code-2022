f0e870c33383e27f1e29c036c46fae553f77b87e
Robert van Drunen <robert@mapspiral.net>
2022-12-05 22:16:05 +0100

    fix: read input data once

--- AdventOfCode.Shared/PuzzleBase{TResult}.cs
+++ AdventOfCode.Shared/PuzzleBase{TResult}.cs
@@ -6,14 +6,16 @@ public abstract class PuzzleBase<TResult>
 {
     public void Solve()
     {
+        var sample = File.ReadAllLines("sample.txt");
+        var input = File.ReadAllLines("input.txt");
         Console.WriteLine($"{GetType()}");
         Console.WriteLine(new String('=', GetType().ToString().Length));
         Console.WriteLine("Solution 1");
-        Console.WriteLine($"\tSample: {Timed(() => Solution1(File.ReadAllLines("sample.txt")))}");
-        Console.WriteLine($"\tInput : {Timed(() => Solution1(File.ReadAllLines("input.txt")))}");
+        Console.WriteLine($"\tSample: {Timed(() => Solution1(sample))}");
+        Console.WriteLine($"\tInput : {Timed(() => Solution1(input))}");
         Console.WriteLine("Solution 2");
-        Console.WriteLine($"\tSample: {Timed(() => Solution2(File.ReadAllLines("sample.txt")))}");
-        Console.WriteLine($"\tInput : {Timed(() => Solution2(File.ReadAllLines("input.txt")))}");
+        Console.WriteLine($"\tSample: {Timed(() => Solution2(sample))}");
+        Console.WriteLine($"\tInput : {Timed(() => Solution2(input))}");
     }
 
     protected virtual TResult? Solution1(IEnumerable<string> input) => default;
